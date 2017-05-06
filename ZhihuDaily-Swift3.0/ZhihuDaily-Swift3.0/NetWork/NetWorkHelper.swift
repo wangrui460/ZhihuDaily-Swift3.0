a5af3ae07bd3203541c1732b4a5e629d19d18c99
wangrui460 <wangruidev@gmail.com>
2017-05-06 16:23:33 +0800

    完成闪屏

--- ZhihuDaily-Swift3.0/ZhihuDaily-Swift3.0/NetWork/NetWorkHelper.swift
+++ ZhihuDaily-Swift3.0/ZhihuDaily-Swift3.0/NetWork/NetWorkHelper.swift
@@ -10,6 +10,7 @@
 // tips：因为Swift存在namespace，所以就不需要写类前缀了
 import UIKit
 import Alamofire
+import SwiftyJSON
 
 // 网络请求超时时间
 let NetworkTimeoutInterval:Double = 10
@@ -17,8 +18,8 @@ let NetworkTimeoutInterval:Double = 10
 @objc protocol NetWorkHelperDelegate:NSObjectProtocol
 {
     // TODO：研究 如果把result的类型改为Any会怎么样
-    @objc optional func netWortDidSuccess(result:AnyObject,requestName:String,parameters:NSDictionary);
-    @objc optional func netWortDidFailed (result:AnyObject,requestName:String,parameters:NSDictionary);
+    @objc optional func netWortDidSuccess(result:AnyObject,requestName:String,parameters:NSDictionary?);
+    @objc optional func netWortDidFailed (result:AnyObject,requestName:String,parameters:NSDictionary?);
 }
 
 class NetWorkHelper: NSObject
@@ -33,20 +34,22 @@ class NetWorkHelper: NSObject
     ///   - url: 可以是字符串，也可以是URL
     ///   - parameters: 参数字典
     ///   - finishedCallback: 完成请求的回调
-    class func request(method:HTTPMethod, url:String, parameters:NSDictionary, finishedCallback:  @escaping (_ result : Any?, _ error: Error?) -> ())
+    class func request(method:HTTPMethod, url:String, parameters:NSDictionary?, finishedCallback:  @escaping (_ result : Any, _ error: Error?) -> ())
     {
         let config:URLSessionConfiguration = URLSessionConfiguration.default
         config.timeoutIntervalForRequest = NetworkTimeoutInterval
         sessionManager = SessionManager(configuration: config)
         sessionManager?.request(url, method: method, parameters: parameters as? Parameters).responseJSON
         { (response) in
+            let data = response.result.value
+            let json = JSON(data as Any)
             if (response.result.isSuccess)
             {
-                finishedCallback(response.result.value, nil)
+                finishedCallback(json, nil)
             }
             else
             {
-                finishedCallback(nil,response.result.error)
+                finishedCallback(json,response.result.error)
             }
         }
     }
@@ -60,20 +63,23 @@ class NetWorkHelper: NSObject
     ///   - requestName: 请求名字，一个成功的代理方法可以处理多个请求，所以用requestName来区分具体请求
     ///   - parameters: 参数字典
     ///   - delegate: 代理对象
-    class func requestDelegate(method:HTTPMethod, url:String, requestName:String, parameters:NSDictionary, delegate:AnyObject)
+    class func requestDelegate(method:HTTPMethod, url:String, requestName:String, parameters:NSDictionary?, delegate:AnyObject)
     {
         let config:URLSessionConfiguration = URLSessionConfiguration.default
         config.timeoutIntervalForRequest = NetworkTimeoutInterval
         sessionManager = SessionManager(configuration: config)
         sessionManager?.request(url, method: method, parameters: parameters as? Parameters).responseJSON
         { (response) in
+            let data = response.result.value
+            let json = JSON(data as Any)
+
             if (response.result.isSuccess)
             {
-                delegate.netWortDidSuccess?(result: response.result.value as AnyObject, requestName: requestName, parameters: parameters)
+                delegate.netWortDidSuccess?(result: json as AnyObject, requestName: requestName, parameters: parameters)
             }
             else
             {
-                delegate.netWortDidFailed?(result: response.result.value as AnyObject, requestName: requestName, parameters: parameters)
+                delegate.netWortDidFailed?(result: json as AnyObject, requestName: requestName, parameters: parameters)
             }
         }
     }
